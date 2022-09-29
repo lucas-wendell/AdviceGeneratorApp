3d647cb95af553b834e766ccfc236c6027bc3389
Lucas Wendell <89931655+l-Wendell@users.noreply.github.com>
2022-09-29 16:47:47 -0300

    add destructuring

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const url = "https://api.adviceslip.com/advice";
 const getAdvice = async () => (await fetch(url)).json();
 
 const addAdviceInDom = async () => {
-	const advice = await getAdvice();
+	const {advice, id} = await getAdvice().slip;
 
 	const $adviceId = document.querySelector('[data-js="adviceId"]');
 	const $advice = document.querySelector('[data-js="advice"]');
 
-	$advice.textContent = `"${advice.slip.advice}"`;
-	$adviceId.textContent = `ADVICE #${advice.slip.id}`;
+	$advice.textContent = `"${advice}"`;
+	$adviceId.textContent = `ADVICE #${id}`;
 };
 
 addAdviceInDom();
